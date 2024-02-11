5a4e4df57a9fc7bfd8ee3c342092c1178e20548d
dotz0ver <opir0oui@gmail.com>
2024-02-11 20:30:07 +0900

    Feat: Create MobileNet v1 Class

--- pytorch-ssd/vision/nn/mobilenet.py
+++ pytorch-ssd/vision/nn/mobilenet.py
@@ -2,4 +2,53 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # MobileNet v1 클래스
-class MobileNetV1(nn.Module)
+class MobileNetV1(nn.Module):
+    def __init__(self, num_classes=1024):
+        super(MobileNetV1, self).__init__()
+
+        #  BN이 있는 Conv 레이어
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+        
+        # DW가 있는 Conv 레이어
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+        
+        # CNN 모델 아키텍처
+        self.model = nn.Sequential(
+            conv_bn(3, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+        )
+        self.fc = nn.Linear(1024, num_classes)
+    
+    # 순전파 함수: 입력을 받아 모델 연산 수행
+    def forward(self, x):
+        x  = self.model(x)
+        x = F.avg_pool2d(x, 7) # 1024 벡터 계산
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        return x
