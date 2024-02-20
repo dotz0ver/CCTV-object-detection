f0d42af28b710bbaa61f189a413fec4cd217398e
dotz0ver <opir0oui@gmail.com>
2024-02-21 03:13:59 +0900

    Feat: Add data transformation classes for training, testing, and prediction

--- pytorch-ssd/vision/ssd/data_preprocessing.py
+++ pytorch-ssd/vision/ssd/data_preprocessing.py
@@ -1,5 +1,6 @@
 from ..transforms.transforms import *
 
+# 1. 훈련 데이터 변환 적용
 class TrainAugmentation:
     def __init__(self, size, mean=0, std=1.0):
         """
@@ -9,15 +10,17 @@ class TrainAugmentation:
         """
         self.mean = mean
         self.size = size
+        # 이미지 데이터 변환
         self.augment = Compose([
-            ConvertFromInts(),
-            PhotometricDistort(),
-            Expand(self.mean),
+            ConvertFromInts(), # 정수 형식을 다른 데이터 타입으로 변환
+            PhotometricDistort(), # 이미지에 포토메트릭 변형을 적용하여 조명 등 다양화
+            Expand(self.mean), # 이미지 확장, 평균값으로 패딩
             RandomSampleCrop(),
-            RandomMirror(),
-            ToPercentCoords(),
-            Resize(self.size),
-            SubtractMeans(self.mean),
+            RandomMirror(), # 랜덤하게 이미지 좌우 반전
+            ToPercentCoords(), # 박스의 좌표를 이미지 크기에 대한 상대 좌표로 변환
+            Resize(self.size), # 이미지를 저장된 크기로 조절
+            SubtractMeans(self.mean), # 이미지 - 평균값
+            # lambda로 이미지를 표준편차로 나누고, 텐서로 변환
             lambda img, boxes=None, labels=None: (img / std, boxes, labels),
             ToTensor(),
         ])
@@ -32,7 +35,7 @@ class TrainAugmentation:
         """
         return self.augment(img, boxes, labels)
 
-
+# 2. 테스트 데이터에 대한 변환
 class TestTransform:
     def __init__(self, size, mean=0.0, std=1.0):
         self.transform = Compose([
@@ -46,7 +49,7 @@ class TestTransform:
     def __call__(self, image, boxes, labels):
         return self.transform(image, boxes, labels)
 
-
+# 3. 모델의 추론을 위한 데이터 변환
 class PredictionTransform:
     def __init__(self, size, mean=0.0, std=1.0):
         self.transform = Compose([
